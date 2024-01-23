0d82f050aeb90ea49828c46486e9263d2a847a70
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-01-23 13:44:41 -0400

    Add: Tailwind + Icofont Remove: generated files when start a new vue project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <header class="header clearfix">
+    <a class="menu_btn" href="javascript:void(0)"><i class="icofont-navigation-menu"></i>Menu</a>
+    <nav class="mainMenu">
+      <ul class="clearfix">
+        <li class="active"><a href="index.html"><i class="icon icon-House"></i><span>Home</span></a></li>
+        <li><a href="resume.html"><i class="icon icon-User"></i><span>Resume</span></a></li>
+        <li><a href="portfolio.html"><i class="icon icon-Bulb"></i><span>Portfolio</span></a></li>
+        <li><a href="blog.html"><i class="icon icon-ClipboardText"></i><span>Blog</span></a></li>
+        <li><a href="contact.html"><i class="icon icon-Imbox"></i><span>Contact</span></a></li>
+      </ul>
+    </nav>
+    <a class="sidebarToggler" href="#"><span><span></span><span></span><span></span></span></a>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="sidebar">
+    <a class="widget_closer" href="javascript:void(0);"><i class="icofont-close-line"></i></a>
+    <div class="widget widget-search">
+      <h3 class="widget_title">Search</h3>
+      <form action="#" class="search_form" method="post">
+        <input name="s" placeholder="Enter Keword" type="text">
+        <button type="submit"><i class="icon icon-Search"></i></button>
+      </form>
+    </div>
+    <div class="widget widget-search">
+      <h3 class="widget_title">Categories</h3>
+      <ul>
+        <li><a href="blog.html">Web</a>(18)</li>
+        <li><a href="blog.html">Startup</a>(11)</li>
+        <li><a href="blog.html">Branding</a>(17)</li>
+        <li><a href="blog.html">UI UX</a>(23)</li>
+        <li><a href="blog.html">Agency</a>(19)</li>
+      </ul>
+    </div>
+    <div class="widget">
+      <h3 class="widget_title">
+        Recent Posts
+      </h3>
+      <div class="singleRecent">
+<!--        <img alt src="assets/images/blog/7.jpg">-->
+        <span>01 Apr, 2021</span>
+        <a href="single_blog.html">
+          Create your next web site with Unity.
+        </a>
+      </div>
+      <div class="singleRecent">
+<!--        <img alt src="assets/images/blog/8.jpg">-->
+        <span>31 Mar, 2021</span>
+        <a href="single_blog.html">
+          Truly unique hundreds of elements.
+        </a>
+      </div>
+      <div class="singleRecent">
+<!--        <img alt src="assets/images/blog/9.jpg">-->
+        <span>26 Mar, 2021</span>
+        <a href="single_blog.html">
+          Easily create unlimited amount of custom.
+        </a>
+      </div>
+    </div>
+    <div class="widget">
+      <h3 class="widget_title">Tags</h3>
+      <div class="tabclouds">
+        <a href="blog.html">Web Design</a>
+        <a href="blog.html">Development</a>
+        <a href="blog.html">CSS</a>
+        <a href="blog.html">Studio</a>
+        <a href="blog.html">Award</a>
+        <a href="blog.html">Mobile</a>
+        <a href="blog.html">IOS</a>
+        <a href="blog.html">IOS</a>
+        <a href="blog.html">UI / UX</a>
+        <a href="blog.html">Portfolio</a>
+      </div>
+    </div>
+  </div>
+  <div class="sidebarOverlay"></div>
+</template>
+
+<style scoped>
+
+</style>
