f4048799f2fd219b02412f5b7fac3c1145fdf3ea
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-01-25 17:07:38 -0400

    Add: Transition betweeen from-to router

--- src/layout/AppMain.vue
+++ src/layout/AppMain.vue
@@ -4,13 +4,26 @@ import {RouterView} from "vue-router";
 </script>
 
 <template>
-<div class="main bg-slate-800 rounded-tl-xl rounded-tr-xl px-3 py-2">
-  <router-view></router-view>
+<div class="main bg-slate-800 rounded-tl-xl rounded-tr-xl p-3">
+  <router-view v-slot="{Component}">
+    <transition name="slide" mode="out-in">
+      <component :is="Component" :key="$route.path"></component>
+    </transition>
+  </router-view>
 </div>
 </template>
 
 <style scoped lang="scss">
 .main{
   min-height: 360px;
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: opacity 300ms, transform 300ms;
+}
+
+.slide-enter-from, .slide-leave-to{
+  opacity: 0;
+  transform: translateY(-30%);
 }
 </style>
