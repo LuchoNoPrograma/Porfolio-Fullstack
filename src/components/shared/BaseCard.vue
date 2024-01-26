dc71b87ab473a2037c84f25c38f75d9f9d13790d
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-01-26 19:32:10 -0400

    Add: BaseCard component

---
+++ src/components/shared/BaseCard.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <div class="p-4 min-w-40 min-h-40" v-bind="$attrs">
+    <slot name="title">
+      <h4 class="text-xl mb-2">
+        {{ props.title }}
+      </h4>
+    </slot>
+
+    <slot>
+      <p>
+        {{ props.description }}
+      </p>
+    </slot>
+  </div>
+</template>
+
+<style scoped></style>
