63adabcf8e57ca7aae84ea6449db59e4f4ac48e2
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-01-26 19:41:03 -0400

    Change: Renamed component to AppHeaderLink.vue

---
+++ src/layout/AppHeaderLink.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import {RouterLink} from "vue-router";
+
+const props = defineProps({
+  iconClass: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  to: {
+    type: String,
+    default: '/about-me',
+  }
+})
+
+</script>
+
+<template>
+  <router-link :to="props.to" active-class="active"
+               class="text-center text-white font-medium py-3 px-2 rounded-xl
+                      flex flex-col flex-wrap items-center hover:text-emerald-200">
+    <i :class="props.iconClass" class="icon text-2xl"></i>
+    <span>{{ props.title }}</span>
+  </router-link>
+</template>
+
+<style scoped>
+
+</style>
