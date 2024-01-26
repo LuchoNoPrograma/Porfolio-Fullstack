15ed97fc6e1052a464b657217c950fa11986db99
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-01-26 19:38:51 -0400

    Feature: ScrollBehavior to top in router.ts

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,6 +3,14 @@ import {MainRoutes} from "./MainRoutes.ts";
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: MainRoutes
+  routes: MainRoutes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {left: 0, top: 0}
+    }
+
+  }
 })
 
