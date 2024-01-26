daf45af6732f17d6cf0ae72fed81c88f095c6bff
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-01-26 19:32:35 -0400

    Add: BaseIcon component + VueTablerIcons

---
+++ src/components/shared/BaseIcon.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import type { TablerIconComponent } from "@tabler/icons-vue";
+import { PropType } from "vue";
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<TablerIconComponent>,
+  },
+  size: {
+    type: Number,
+    default: 20,
+  },
+  strokeWidth: {
+    type: Number,
+    default: 1.5,
+  },
+});
+</script>
+
+<template>
+  <component
+    :is="props.item"
+    :size="props.size"
+    :stroke-width="props.strokeWidth"
+    class="inline"
+  ></component>
+</template>
