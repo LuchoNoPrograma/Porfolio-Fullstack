b2ccb4c9695a84b8abd9bedf5e33c18ef5f56222
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-01-26 19:34:11 -0400

    Add: CustomAccordion + CustomAccordionItem Component (Timeline included)

--- src/components/shared/CustomAccordion.vue
+++ src/components/shared/CustomAccordion.vue
@@ -1,25 +1,33 @@
 <script lang="ts" setup>
-import {PropType} from "vue";
-import type {ItemAccordion} from "../../types/type-components.ts";
+import { PropType } from "vue";
+import type { ItemAccordion } from "../../types/type-components.ts";
 import CustomAccordionItem from "./CustomAccordionItem.vue";
 
 const props = defineProps({
   items: {
     type: Array as PropType<Array<ItemAccordion>>,
-    required: true
-  }
-})
-
+    required: true,
+  },
+});
 </script>
 
 <template>
-  <div>
+  <div class="relative" v-bind="$attrs">
+    <div
+      class="absolute left-2 transform -translate-x-1/2 h-full w-1 bg-emerald-400"
+    ></div>
     <template v-for="(item, index) in props.items" :key="index">
-      <custom-accordion-item :index="index" :item="item"></custom-accordion-item>
+      <custom-accordion-item
+        :index="index"
+        :item="item"
+        :items-length="items.length"
+      ></custom-accordion-item>
     </template>
   </div>
+
+  <!--  DOTS-->
+  <!--  <div v-if="index !== 0"
+         class="absolute left-0 top-0 bottom-0 transform -translate-x-1/2 h-4 w-4 bg-emerald-400 rounded-full"></div>-->
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/shared/CustomAccordionItem.vue
+++ src/components/shared/CustomAccordionItem.vue
@@ -1,59 +1,77 @@
 <script lang="ts" setup>
-import {onMounted, PropType, ref} from "vue";
-import {ItemAccordion} from "../../types/type-components.ts";
+import { PropType, ref } from "vue";
+import { ItemAccordion } from "../../types/type-components.ts";
 
 const props = defineProps({
   item: {
     type: Object as PropType<ItemAccordion>,
-    required: true
+    required: true,
   },
   index: {
     type: Number,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
-const accordionContent = ref<HTMLDivElement>()
+const accordionContent = ref<HTMLDivElement>();
 
 const toggleAccordion = () => {
-  props.item.expanded = !props.item.expanded
+  props.item.expanded = !props.item.expanded;
   console.log(accordionContent.value.clientHeight);
-}
-
+};
 </script>
 
 <template>
-  <div class="mt-3">
-    <h2 class="mb-2"  @click="toggleAccordion">
-      <button :aria-controls="'accordion-flush-body-' + index"
-              :aria-expanded="!item?.expanded"
-              class="w-full flex items-center justify-between gap-3 py-1 font-semibold text-gray-300 border-b border-emerald-400 z-0 cursor-pointer"
-              type="button">
-        <span>{{ item.title }}</span>
-        <svg :class="item?.expanded? 'rotate-180' : 'rotate-0'"
-             aria-hidden="true"
-             class="w-3 h-3 shrink-0 transition-all duration-300 ease-in "
-             data-accordion-icon
-             fill="none" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
-          <path d="M9 5 5 1 1 5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="2"/>
+  <div class="mt-4 ml-5 relative">
+    <h2 class="mb-2" @click="toggleAccordion">
+      <button
+        :aria-controls="'accordion-flush-body-' + index"
+        :aria-expanded="!item?.expanded"
+        class="w-full flex items-center justify-between gap-3 py-1 font-semibold text-gray-300 text-left border-b border-emerald-400 z-0 cursor-pointer relative"
+        type="button"
+      >
+        <span
+          class="dot absolute bottom-1 transform -translate-x-1/2 h-4 w-4 bg-emerald-400 rounded-full"
+        ></span>
+        <span class="text-gray-200">{{ item.title }}</span>
+        <svg
+          :class="item?.expanded ? 'rotate-180' : 'rotate-0'"
+          aria-hidden="true"
+          class="w-3 h-3 shrink-0 transition-all duration-300 ease-in relative"
+          data-accordion-icon
+          fill="none"
+          viewBox="0 0 10 6"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M9 5 5 1 1 5"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          />
         </svg>
       </button>
     </h2>
-
-
-    <div :aria-labelledby="'accordion-content' + index"
-         :class="item.expanded? 'expanded' : 'collapsed'"
-         ref="accordionContent"
+    <div
+      ref="accordionContent"
+      :aria-labelledby="'accordion-content' + index"
+      :class="item.expanded ? 'expanded' : 'collapsed'"
+      class="pb-2 relative"
     >
-      <div v-if="item.chips" class="accordion-content__container-chip w-full flex justify-start flex-wrap gap-2 mb-1 ml-1">
-        <span v-for="(chip, index) in item.chips" :key="index"
-              class="text-cyan-900 bg-emerald-400 px-3 rounded-xl">
-          {{ chip.title }}
+      <div
+        v-if="item.chips"
+        class="accordion-content__container-chip w-full flex justify-start flex-wrap gap-2 mb-1 ml-1"
+      >
+        <span
+          v-for="(chip, index) in item.chips"
+          :key="index"
+          class="bg-emerald-800 px-3 rounded-2xl"
+        >
+          <span class="text-emerald-200">{{ chip.title }}</span>
         </span>
       </div>
-
-      <span class="text-gray-400">{{ item.content }}</span>
+      <span class="text-gray-300">{{ item.content }}</span>
     </div>
   </div>
 </template>
@@ -62,18 +80,21 @@ const toggleAccordion = () => {
 .collapsed {
   opacity: 0;
   max-height: 0;
-  transition: all 300ms linear;
+  transition: all 400ms linear;
   overflow: hidden;
 }
 
 .expanded {
   opacity: 1;
   max-height: 100px;
-  transition: all 300ms linear;
+  transition: all 400ms linear;
 }
 
 .rotate-180 {
   transform: rotate(180deg);
 }
 
+.dot {
+  left: -0.875em;
+}
 </style>
