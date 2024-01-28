129e6f308f76c14c2eea89b52a7c5a6761200525
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-01-28 19:46:20 -0400

    Change: scrollBehavior for router is changed, now its go to element in App.vue with id="page-top" with smooth animation

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,12 +5,7 @@ export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: MainRoutes,
   scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return {left: 0, top: 0}
-    }
-
+    return {el : '#page-top', top: 0, behavior: 'smooth'}
   }
 })
 
