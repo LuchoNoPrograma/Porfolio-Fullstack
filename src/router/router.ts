b829e0c2b1719c2fadb1ed921766bd54cad582f1
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-02-01 15:12:07 -0400

    Fix: add typed router.ts

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,7 +1,7 @@
-import {createRouter, createWebHistory} from "vue-router";
+import {createRouter, createWebHistory, Router} from "vue-router";
 import {MainRoutes} from "./MainRoutes.ts";
 
-export const router = createRouter({
+export const router: Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: MainRoutes,
   scrollBehavior(to, from, savedPosition) {
