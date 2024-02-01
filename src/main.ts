d7dfc4ac86448aac656418726700a8ef77654ad7
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-02-01 15:00:43 -0400

    Add: main.ts now is using dependencies (pinia, vueViewer)

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,35 @@
-import {createApp} from 'vue'
+import { createApp } from "vue"; //@ts-ignore
+import Ripple from "vue3-whr-ripple-directive/src/ripple.js";
+import VueTablerIcons from "vue-tabler-icons";
 
-//@ts-ignore
-import Ripple from 'vue3-whr-ripple-directive/src/ripple.js';
-import VueTablerIcons from 'vue-tabler-icons';
-
-import './assets/scss/style.scss';
-import './assets/css/icofont.css';
-import './assets/css/stroke-gap-icons.css';
+import "./assets/scss/style.scss";
+import "./assets/css/icofont.css";
+import "./assets/css/stroke-gap-icons.css";
 import App from "./App.vue";
-import {router} from "./router/router.ts";
+import { router } from "./router/router.ts";
+import { createPinia } from "pinia";
+import "viewerjs/dist/viewer.css";
+import VueViewer from "v-viewer";
 
 
+import "./assets/scss/override.scss";
+
+const pinia = createPinia();
 const app = createApp(App);
-app.directive('ripple', Ripple);
-Ripple.color = 'rgba(255, 255, 255, 0.35)';
-Ripple.zIndex = 55;
 
-app.use(VueTablerIcons)
-app.use(router)
-app.mount('#app');
+app.use(router);
+app.use(pinia);
+app.use(VueTablerIcons);
+app.use(VueViewer, {
+  defaultOptions: {
+    rotatable: false
+  }
+});
+
+Ripple.color = "rgba(255, 255, 255, 0.35)";
+Ripple.zIndex = 55;
+app.directive("ripple", Ripple);
+
+app.mount("#app");
 
 export default app;
-
