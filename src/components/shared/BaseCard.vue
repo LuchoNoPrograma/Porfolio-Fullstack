630a4a3a4b75aeee4882bdbcd750e95f4dd8e8b0
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-02-01 15:04:03 -0400

    Add: BaseCard.vue with padding, slot title, slot-default, slot-actions + color now is available

--- src/components/shared/BaseCard.vue
+++ src/components/shared/BaseCard.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { PropType } from "vue";
+
 const props = defineProps({
   title: {
     type: String,
@@ -8,22 +10,50 @@ const props = defineProps({
     type: String,
     default: "",
   },
+  color: {
+    type: String,
+    default: "primary",
+  },
+  padding: {
+    type: [String, Number],
+    default: 4,
+  },
 });
 </script>
 
 <template>
-  <div class="p-4 min-w-40 min-h-40" v-bind="$attrs">
-    <slot name="title">
-      <h4 class="text-xl mb-2">
-        {{ props.title }}
-      </h4>
-    </slot>
+  <div
+    :class="[
+      {
+        'bg-emerald-850': props.color === 'primary',
+        'bg-cyan-850': props.color === 'secondary',
+      },
+      `p-${padding}`,
+    ]"
+    class="min-w-40 min-h-40"
+    v-bind="$attrs"
+  >
+    <div>
+      <slot name="title">
+        <h4 class="text-xl mb-2">
+          {{ props.title }}
+        </h4>
+      </slot>
+    </div>
 
-    <slot>
-      <p>
-        {{ props.description }}
-      </p>
-    </slot>
+    <div v-if="$.slots.default" class="mb-2">
+      <slot>
+        <p>
+          {{ props.description }}
+        </p>
+      </slot>
+    </div>
+
+    <div v-if="$.slots.actions" class="mt-4 mb-1">
+      <slot name="actions">
+
+      </slot>
+    </div>
   </div>
 </template>
 
