6a22e6a8e89b688ac1c8f607ca492a397dd610db
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-02-01 14:59:57 -0400

    Add: ItemsNavigations.ts value

---
+++ src/data/ItemNavigation.ts
@@ -0,0 +1,29 @@
+import {ItemNavigationType} from "../types/DataType.ts";
+
+export const itemsNavigation: ItemNavigationType[] = [
+  {
+    iconClass: "icon-House",
+    title: "Sobre mi",
+    to: "/about-me",
+  },
+  {
+    iconClass: "icon-User",
+    title: "Resúmen",
+    to: "/resume",
+  },
+  {
+    iconClass: "icon-Bulb",
+    title: "Proyectos",
+    to: "/projects",
+  },
+  {
+    iconClass: "icon-ClipboardText",
+    title: "Habilidades",
+    // to: "/blog",
+  },
+  {
+    iconClass: "icon-Imbox",
+    title: "Contactos",
+    // to: "/contact",
+  },
+];
